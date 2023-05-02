6d4126082b6767156c6164bbb6e3d3f6959fba5a
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-02 13:29:43 -0400

    Added initialize to LightsOutBoard
    Had to add to_s and a couple helper methods to facilitate the function and testing.

--- libs/games/lights_out_board.rb
+++ libs/games/lights_out_board.rb
@@ -10,5 +10,49 @@ module Games
       return false unless seed.chars.all? { |ch| CHARACTER_VALUES.include? ch }
       true
     end
+
+    def initialize(seed)
+      unless LightsOutBoard.valid_seed? seed
+        raise ArgumentError.new "Invalid Seed"
+      end
+      @seed = seed
+      @lights = from_seed seed
+    end
+
+    def to_s
+      to_seed
+    end
+
+    private
+
+    def from_seed(seed)
+      seed.chars.each_with_object([]) do |character, board|
+        value = CHARACTER_VALUES.index character
+        board << lights_row(value)
+      end
+    end
+
+    def lights_row(value)
+      row = []
+
+      until value == 0
+        value, remainder = value.divmod 2
+        row.unshift(remainder == 1)
+      end
+
+      row
+    end
+
+    def to_seed
+      @lights.map do |row|
+        value = 0
+
+        row.reverse_each.with_index do |light, idx|
+          value += 2**idx if light
+        end
+
+        CHARACTER_VALUES[value]
+      end.join
+    end
   end
 end
--- test/lights_out_board_test.rb
+++ test/lights_out_board_test.rb
@@ -8,4 +8,13 @@ class LightsOutBoardTest < Minitest::Test
     refute Games::LightsOutBoard.valid_seed?('pkmzd')
     refute Games::LightsOutBoard.valid_seed?('000000')
   end
+
+  def test_to_s
+    lights = Games::LightsOutBoard.new('0u3dj')
+    assert_equal '0u3dj', lights.to_s
+  end
+
+  def test_initialize_error
+    assert_raises(ArgumentError) { Games::LightsOutBoard.new('012345') }
+  end
 end
