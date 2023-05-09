8c6c9f10cfe48f75353e0495c94142b8cddcc9d3
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-08 20:42:21 -0400

    Updated LightsOutBoardTest for 6x6

--- test/lights_out_board_test.rb
+++ test/lights_out_board_test.rb
@@ -4,15 +4,15 @@ require_relative '../libs/games/lights_out_board'
 
 class LightsOutBoardTest < Minitest::Test
   def test_valid_seed?
-    assert Games::LightsOutBoard.valid_seed?('0u3dj')
-    refute Games::LightsOutBoard.valid_seed?('pkmzd')
-    refute Games::LightsOutBoard.valid_seed?('000000')
-    refute Games::LightsOutBoard.valid_seed?('pkm')
+    assert Games::LightsOutBoard.valid_seed?('0f3a2b08101c')
+    refute Games::LightsOutBoard.valid_seed?('lol')
+    refute Games::LightsOutBoard.valid_seed?('0000000000000')
+    refute Games::LightsOutBoard.valid_seed?('403a2b08101c')
   end
 
   def test_to_s
-    lights = Games::LightsOutBoard.new '0u3dj'
-    assert_equal '0u3dj', lights.to_s
+    lights = Games::LightsOutBoard.new '0f3a2b08101c'
+    assert_equal '0f3a2b08101c', lights.to_s
   end
 
   def test_initialize_error
@@ -20,22 +20,22 @@ class LightsOutBoardTest < Minitest::Test
   end
 
   def test_move
-    board = Games::LightsOutBoard.new 'oi720'
+    board = Games::LightsOutBoard.new '030100103810'
     board.move! 0
-    assert_equal '02720', board.to_s
-    board.move! 13
-    assert_equal '00000', board.to_s
+    assert_equal '000000103810', board.to_s
+    board.move! 28
+    assert_equal '000000000000', board.to_s
   end
 
   def test_move_error
-    board = Games::LightsOutBoard.new 'uuuuu'
+    board = Games::LightsOutBoard.new '3f3f3f3f3f3f'
     assert_raises(IndexError) { board.move!(-1) }
-    assert_raises(IndexError) { board.move!(25) }
+    assert_raises(IndexError) { board.move!(36) }
   end
 
   def test_initial_seed
-    board = Games::LightsOutBoard.new 'fight'
-    assert_equal 'fight', board.initial_seed
+    board = Games::LightsOutBoard.new '0f3a2b08101c'
+    assert_equal '0f3a2b08101c', board.initial_seed
   end
 
   def test_random_seed
@@ -46,7 +46,7 @@ class LightsOutBoardTest < Minitest::Test
   end
 
   def test_win
-    board = Games::LightsOutBoard.new '00000'
+    board = Games::LightsOutBoard.new '000000000000'
     assert_equal true, board.win?
     board.move! 0
     assert_equal false, board.win?
